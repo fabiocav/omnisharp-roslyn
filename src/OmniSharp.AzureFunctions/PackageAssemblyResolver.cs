9aedd8273e3c8b30e12afa74706c6d934e51a3ec
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2016-07-01 11:22:40 -0700

    [Partial] Assembly resolution

---
+++ src/OmniSharp.AzureFunctions/PackageAssemblyResolver.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OmniSharp.AzureFunctions
+{
+    // Copyright (c) .NET Foundation. All rights reserved.
+    // Licensed under the MIT License. See License.txt in the project root for license information.
+
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using Newtonsoft.Json.Linq;
+
+    namespace Microsoft.Azure.WebJobs.Script.Description
+    {
+        /// <summary>
+        /// Resolves assemblies referenced by NuGet packages used by the function.
+        /// </summary>
+        public sealed class PackageAssemblyResolver
+        {
+            private const string EmptyFolderFileMarker = "_._";
+            private const string FrameworkTargetName = ".NETFramework,Version=v4.6";
+
+            private readonly ImmutableArray<PackageReference> _packages;
+
+            public PackageAssemblyResolver(string functionDirectory)
+            {
+                _packages = InitializeAssemblyRegistry(functionDirectory);
+            }
+
+            public ImmutableArray<PackageReference> Packages
+            {
+                get
+                {
+                    return _packages;
+                }
+            }
+
+            public IEnumerable<string> AssemblyReferences
+            {
+                get
+                {
+                    return _packages.Aggregate(new List<string>(), (assemblies, p) =>
+                    {
+                        assemblies.AddRange(p.Assemblies.Values);
+                        assemblies.AddRange(p.FrameworkAssemblies.Values);
+                        return assemblies;
+                    });
+                }
+            }
+
+            private static ImmutableArray<PackageReference> InitializeAssemblyRegistry(string functionDirectory)
+            {
+                var builder = ImmutableArray<PackageReference>.Empty.ToBuilder();
+                string fileName = Path.Combine(functionDirectory, "project.lock.json");
+
+                if (File.Exists(fileName))
+                {
+                    var jobject = JObject.Parse(File.ReadAllText(fileName));
+
+                    var target = jobject.SelectTokens(string.Format(CultureInfo.InvariantCulture, "$.targets['{0}']", FrameworkTargetName)).FirstOrDefault();
+
+                    if (target != null)
+                    {
+                        string nugetHome = PackageManager.GetNugetPackagesPath();
+
+                        foreach (JProperty token in target)
+                        {
+                            var referenceNameParts = token.Name.Split('/');
+                            if (referenceNameParts.Length != 2)
+                            {
+                                throw new FormatException(string.Format(CultureInfo.InvariantCulture, "The package name '{0}' is not correctly formatted.", token.Name));
+                            }
+
+                            var package = new PackageReference(referenceNameParts[0], referenceNameParts[1]);
+
+                            var references = token.SelectTokens("$..compile").FirstOrDefault();
+                            if (references != null)
+                            {
+                                foreach (JProperty reference in references)
+                                {
+                                    if (!reference.Name.EndsWith(EmptyFolderFileMarker, StringComparison.Ordinal))
+                                    {
+                                        string path = Path.Combine(nugetHome, token.Name, reference.Name);
+                                        path = path.Replace('/', '\\');
+
+                                        if (File.Exists(path))
+                                        {
+                                            package.Assemblies.Add(AssemblyName.GetAssemblyName(path), path);
+                                        }
+                                    }
+                                }
+                            }
+
+                            var frameworkAssemblies = token.SelectTokens("$..frameworkAssemblies").FirstOrDefault();
+                            if (frameworkAssemblies != null)
+                            {
+                                foreach (var assembly in frameworkAssemblies)
+                                {
+                                    string assemblyName = assembly.ToString();
+                                    package.FrameworkAssemblies.Add(new AssemblyName(assemblyName), assemblyName);
+                                }
+                            }
+
+                            builder.Add(package);
+                        }
+                    }
+                }
+
+                return builder.ToImmutableArray();
+            }
+
+            public bool TryResolveAssembly(string name, out string path)
+            {
+                path = null;
+                var assemblyName = new AssemblyName(name);
+
+                foreach (var package in _packages)
+                {
+                    if (package.Assemblies.TryGetValue(assemblyName, out path) ||
+                        package.FrameworkAssemblies.TryGetValue(assemblyName, out path))
+                    {
+                        break;
+                    }
+                }
+
+                return path != null;
+            }
+        }
+    }
+
+    public class PackageReference
+    {
+        public PackageReference(string name, string version)
+        {
+            Name = name;
+            Version = version;
+            Assemblies = new Dictionary<AssemblyName, string>(new AssemblyNameEqualityComparer());
+            FrameworkAssemblies = new Dictionary<AssemblyName, string>(new AssemblyNameEqualityComparer());
+        }
+
+        public Dictionary<AssemblyName, string> Assemblies { get; private set; }
+
+        public Dictionary<AssemblyName, string> FrameworkAssemblies { get; private set; }
+
+        public string Name { get; private set; }
+
+        public string Version { get; private set; }
+
+        private class AssemblyNameEqualityComparer : IEqualityComparer<AssemblyName>
+        {
+            public bool Equals(AssemblyName x, AssemblyName y)
+            {
+                if (x == y)
+                {
+                    return true;
+                }
+
+                if (x == null || y == null)
+                {
+                    return false;
+                }
+
+                return string.Compare(x.FullName, y.FullName, StringComparison.OrdinalIgnoreCase) == 0;
+            }
+
+            public int GetHashCode(AssemblyName obj)
+            {
+                if (obj == null || obj.FullName == null)
+                {
+                    return 0;
+                }
+
+                return obj.FullName.GetHashCode();
+            }
+        }
+    }
+}
---
+++ src/OmniSharp.AzureFunctions/PackageReference.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OmniSharp.AzureFunctions
+{
+    /// <summary>
+    /// Provides NuGet package management functionality.
+    /// </summary>
+    internal sealed class PackageManager
+    {
+        private const string NugetPathEnvironmentKey = "AzureWebJobs_NuGetPath";
+        private const string NuGetFileName = "nuget.exe";
+
+        //public PackageManager(FunctionMetadata metadata, TraceWriter traceWriter)
+        //{
+        //    _functionMetadata = metadata;
+        //    _traceWriter = traceWriter;
+        //}
+
+        public Task RestorePackagesAsync()
+        {
+            var tcs = new TaskCompletionSource<bool>();
+
+            try
+            {
+                string functionDirectory = Path.GetDirectoryName("");
+                string projectPath = Path.Combine(functionDirectory, "project.json");
+                string nugetHome = GetNugetPackagesPath();
+
+                var startInfo = new ProcessStartInfo
+                {
+                    FileName = ResolveNuGetPath(),
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true,
+                    UseShellExecute = false,
+                    ErrorDialog = false,
+                    WorkingDirectory = functionDirectory,
+                    Arguments = string.Format(CultureInfo.InvariantCulture, "restore \"{0}\" -PackagesDirectory \"{1}\"", projectPath, nugetHome)
+                };
+
+                var process = new Process { StartInfo = startInfo };
+                process.ErrorDataReceived += ProcessDataReceived;
+                process.OutputDataReceived += ProcessDataReceived;
+                process.EnableRaisingEvents = true;
+
+                process.Exited += (s, e) =>
+                {
+                    tcs.SetResult(process.ExitCode == 0);
+                    process.Close();
+                };
+
+                process.Start();
+
+                process.BeginErrorReadLine();
+                process.BeginOutputReadLine();
+            }
+            catch (Exception exc)
+            {
+                tcs.SetException(exc);
+            }
+
+            return tcs.Task;
+        }
+
+        public static string ResolveNuGetPath()
+        {
+            // Check if we have the path in the well known environment variable
+            string path = Environment.GetEnvironmentVariable(NugetPathEnvironmentKey);
+
+            //// If we don't have the path, try to get a fully qualified path to Kudu's NuGet copy.
+            if (string.IsNullOrEmpty(path))
+            {
+                // Get the latest Kudu extension path
+                string kuduPath = Directory.GetDirectories(Environment.ExpandEnvironmentVariables("%programfiles(x86)%\\siteextensions\\kudu"))
+                    .OrderByDescending(d => d).FirstOrDefault();
+
+                if (!string.IsNullOrEmpty(kuduPath))
+                {
+                    path = Path.Combine(kuduPath, "bin\\scripts", NuGetFileName);
+                }
+            }
+
+            // Return the resolved value or expect NuGet.exe to be present in the path.
+            return path ?? NuGetFileName;
+        }
+
+        internal static string GetNugetPackagesPath()
+        {
+            string nugetHome = null;
+            string home = Environment.GetEnvironmentVariable("HOME");
+            if (!string.IsNullOrEmpty(home))
+            {
+                // We're hosted in Azure
+                // Set the NuGet path to %home%\data\Functions\packages\nuget
+                // (i.e. d:\home\data\Functions\packages\nuget)
+                nugetHome = Path.Combine(home, "data\\Functions\\packages\\nuget");
+            }
+            else
+            {
+                string userProfile = Environment.ExpandEnvironmentVariables("%userprofile%");
+                nugetHome = Path.Combine(userProfile, ".nuget\\packages");
+            }
+
+            return nugetHome;
+        }
+
+        private void ProcessDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            
+        }
+    }
+}
